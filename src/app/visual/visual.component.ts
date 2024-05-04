920d56f4a27f998549f3bc9b16927dcdc724deec
Sebastian Mark <smarkgolfer@gmail.com>
2024-05-04 15:57:00 -0400

    Cleaned up the visual components class

--- src/app/visual/visual.component.ts
+++ src/app/visual/visual.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, AfterViewInit, Renderer2, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
+import { Component} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
@@ -20,51 +19,10 @@ declare const window: any;
     TableauEmbeddedVizComponent
   ]
 })
-export class VisualComponent implements OnInit, AfterViewInit {
-  graph1: boolean = false;
-  graph2: boolean = false;
-  graph3: boolean = false;
-  graph4: boolean = false;
-
-  constructor(private renderer: Renderer2, private el: ElementRef, @Inject(PLATFORM_ID) private platformId: Object) {}
-
-  ngOnInit(): void {}
-
-  ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.loadTableauScript();
-    }
-  }
-
-  private loadTableauScript() {
-    if (window.tableau) {
-      this.initTableau();
-    } else {
-      const script = this.renderer.createElement('script');
-      script.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
-      script.onload = () => {
-        this.initTableau();
-      };
-      script.onerror = () => {
-        console.error('Script loading failed.');
-      };
-      this.renderer.appendChild(document.head, script);
-    }
-  }
-
-  private initTableau(): void {
-    if (window.tableau) {
-      const vizUrl = 'https://public.tableau.com/views/DayStrainVSSleepPerfomance/DayStrainVSSleepPerformance';
-      const options = {
-        width: '100%',
-        height: '500px',
-        onFirstInteractive: function() {
-          console.log('Tableau visualization is interactive.');
-        }
-      };
-      new window.tableau.Viz(this.el.nativeElement.querySelector('#viz1714176135384'), vizUrl, options);
-    } else {
-      console.error('Tableau is not defined.');
-    }
-  }
+export class VisualComponent {
+  
+  selectedGraph: number = 0; 
+  showTableauViz: boolean = false;
+  
 }
+
